0e21d2b85c3cc934b0490038621a25bb2ce5b158
Brian Rosner <brosner@eldarion.com>
2016-03-30 14:04:05 -0600

    Use custom buildpack

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -4,4 +4,4 @@ set -ev
 here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 . $here/_common.sh
 
-git archive --format=tar "$TRAVIS_COMMIT" | docker run -i $BUILDER_IMAGE - > $BUNDLE_FILE
+git archive --format=tar "$TRAVIS_COMMIT" | docker run -i -e BUILDPACK_URL=https://github.com/kelproject/system-go-buildpack.git $BUILDER_IMAGE - > $BUNDLE_FILE
